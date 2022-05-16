b184d8ac15da5d4bd661cd4ace749b8ed4a4730a
Dmitri Yatsenko <dmitriyatsenko1995@gmail.com>
2022-05-16 20:24:03 +0000

    multiple strings

--- W1D1/reverse.js
+++ W1D1/reverse.js
@@ -1,9 +1,12 @@
-let string = process.argv[2];
+// let string = process.argv[2];
+let string = process.argv.slice(2);
 
 function reverse(string) {
     let reverse = "";
-    for (let i = string.length - 1; i >= 0; i--) {
-        reverse += string[i];
+    for (let i = 0; i < string.length; i++) {
+        for (let j = string[i].length - 1; j >= 0; j--) {
+            reverse += string[i][j];
+        }
     }
     return reverse;
 }
